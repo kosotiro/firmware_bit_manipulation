82b2c70efbe2bd106adc6060efc6ca7b805cec14
kosotiro <40856955+kosotiro@users.noreply.github.com>
2025-10-26 21:03:38 +0200

    Add Doxygen documentation

--- main.c
+++ main.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+/* Set or Clear a Specific Bit in a Register
+*  Write a C program to set or clear a specific bit in an 8-bit register based on user input.
+*
+* The user provides an 8-bit integer (register value), a bit position (0-7), and a
+* mode (0 for clear, 1 for set).
+* Your task is to modify the register value accordingly and print the updated value. */
+
+/**
+ * Set or clear a specific bit in an 8-bit register based on user input
+ * @param[in] reg 8-bit register
+ * @param[in] pos position for change
+ * @param[in] mode clear or set on this position
+ * @param[out] area
+ */
 unsigned char modifyBit(unsigned char reg, int pos, int mode) {
     /* clear the bit */
     if (!mode)
